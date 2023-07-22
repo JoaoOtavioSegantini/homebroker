84b0af54d3ad073150871e49ee161d823502ee04
João Otávio <joaotavioos@outlook.com>
2023-07-22 06:43:48 -0300

    feat: add assets service method create ✨

--- nestjs/src/assets/assets.service.ts
+++ nestjs/src/assets/assets.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
-export class AssetsService {}
+export class AssetsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: { id: string; symbol: string; price: number }) {
+    return this.prisma.asset.create({
+      data,
+    });
+  }
+}
