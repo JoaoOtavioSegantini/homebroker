dd7312bf93d366735309241703e94a9069cad79c
João Otávio <joaotavioos@outlook.com>
2023-07-22 18:42:16 -0300

    feat: add wallets service methods ✨

--- nestjs/src/wallets/wallets.service.ts
+++ nestjs/src/wallets/wallets.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
-export class WalletsService {}
+export class WalletsService {
+  constructor(private prisma: PrismaService) {}
+
+  all() {
+    return this.prisma.wallet.findMany();
+  }
+
+  create(input: { id: string }) {
+    return this.prisma.wallet.create({
+      data: {
+        id: input.id,
+      },
+    });
+  }
+}
