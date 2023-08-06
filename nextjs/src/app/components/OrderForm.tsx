ad19b1f6c392bd4231378359e217799af7900894
João Otávio <joaotavioos@outlook.com>
2023-08-06 15:57:02 -0300

    feat: add flowbite components in OrderForm compoment ✨

--- nextjs/src/app/components/OrderForm.tsx
+++ nextjs/src/app/components/OrderForm.tsx
@@ -1,58 +1,85 @@
+import { Label, TextInput, Button } from "../components/flowbite-components";
 import { revalidateTag } from "next/cache";
 
 async function initTransaction(formData: FormData) {
-    "use server";
-    const shares = formData.get("shares");
-    const price = formData.get("price");
-    const wallet_id = formData.get("wallet_id");
-    const asset_id = formData.get("asset_id");
-    const type = formData.get("type");
+  "use server";
+  const shares = formData.get("shares");
+  const price = formData.get("price");
+  const wallet_id = formData.get("wallet_id");
+  const asset_id = formData.get("asset_id");
+  const type = formData.get("type");
 
-    const response = await fetch(
-        `http://localhost:8000/wallets/${wallet_id}/orders`,
-        {
-            headers: { "Content-type": "application/json" },
-            method: "POST",
-            body: JSON.stringify({
-                shares,
-                price,
-                asset_id,
-                type,
-                status: "OPEN",
-                Asset: { id: asset_id, symbol: "petro4", price: 50 },
-            }),
-        }
-    );
-    revalidateTag(`orders-wallet-${wallet_id}`);
-    return await response.json();
+  const response = await fetch(
+    `http://host.docker.internal:3000/wallets/${wallet_id}/orders`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        shares,
+        price,
+        asset_id,
+        type,
+        status: "OPEN",
+        Asset: {
+          id: asset_id,
+          symbol: "PETR4",
+          price: 30,
+        },
+      }),
+    }
+  );
+  revalidateTag(`orders-wallet-${wallet_id}`);
+  return await response.json();
 }
 
-export function OrderForm(props: { asset_id: string; wallet_id: string }) {
-    return (
+export function OrderForm(props: {
+  asset_id: string;
+  wallet_id: string;
+  type: "BUY" | "SELL";
+}) {
+  return (
+    <div>
+      <h1>Order Form</h1>
+      <form action={initTransaction}>
+        <input name="asset_id" type="hidden" defaultValue={props.asset_id} />
+        <input name="wallet_id" type="hidden" defaultValue={props.wallet_id} />
+        <input name="type" type="hidden" defaultValue={"BUY"} />
         <div>
-            <h1>Order Form</h1>
-            <form action={initTransaction}>
-                <input name="asset_id" type="hidden" defaultValue={props.asset_id} />
-                <input name="wallet_id" type="hidden" defaultValue={props.wallet_id} />
-                <input name="type" type="hidden" defaultValue={"BUY"} />
-                <input
-                    name="shares"
-                    type="number"
-                    min={1}
-                    step={1}
-                    placeholder="quantidade"
-                />
-                <br />
-                <input
-                    name="price"
-                    type="number"
-                    min={1}
-                    step={0.1}
-                    placeholder="preço"
-                />
-                <br />
-                <button>Comprar</button>
-            </form>
+          <div className="mb-2 block">
+            <Label htmlFor="shares" value="Quantidade" />
+          </div>
+          <TextInput
+            id="shares"
+            name="shares"
+            required
+            type="number"
+            min={1}
+            step={1}
+            defaultValue={1}
+          />
         </div>
-    );
+        <br />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="shares" value="Preço R$" />
+          </div>
+          <TextInput
+            id="price"
+            name="price"
+            required
+            type="number"
+            min={1}
+            step={1}
+            defaultValue={1}
+          />
+        </div>
+        <br />
+        <Button type="submit" color={props.type === "BUY" ? "green" : "red"}>
+          Confirmar {props.type === "BUY" ? "compra" : "venda"}
+        </Button>
+      </form>
+    </div>
+  );
 }
